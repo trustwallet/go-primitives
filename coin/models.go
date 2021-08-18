09f9d9b9ca3c55a32254e9e99309725a91bc8b7f
Ivan Bozhytskyi <82358585+ivan-bozhytksyi@users.noreply.github.com>
2021-08-19 01:02:00 +0300

    Import packages from golibs

---
+++ coin/models.go
@@ -0,0 +1,12 @@
+package coin
+
+import "errors"
+
+func GetCoinForId(id string) (Coin, error) {
+	for _, c := range Coins {
+		if c.Handle == id {
+			return c, nil
+		}
+	}
+	return Coin{}, errors.New("unknown id: " + id)
+}
