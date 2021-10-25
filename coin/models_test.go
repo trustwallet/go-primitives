aa0b47d5907250e05480d62e53131fbe879eb74a
Semyon <semyonL1995@gmail.com>
2021-10-25 19:40:48 +0300

    Added utilities functions to get explore url for chain and get chain from asset type. (#6)
    * Added func to get explore url for coin; added func to get get chain from asset type
    
    * fix case
    
    * moved get chein from asset type to types; added more test; replaced chain(string) param to coin.Coin
    
    * fixes according review
    
    * renamed param
    
    * goimports applied, fixes
    
    * added roinin coin data
    
    Co-authored-by: semen.loboichenko <semen.loboichenko@koalaplatform.com>

--- coin/models_test.go
+++ coin/models_test.go
@@ -37,3 +37,66 @@ func TestGetCoinForId(t *testing.T) {
 		})
 	}
 }
+
+func TestGetCoinExploreURL(t *testing.T) {
+	type args struct {
+		addr  string
+		chain Coin
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "Test ethereum",
+			args: args{
+				addr:  "token",
+				chain: Ethereum(),
+			},
+			want:    "https://etherscan.io/token/token",
+			wantErr: false,
+		},
+		{
+			name: "Test custom chain",
+			args: args{
+				addr:  "token",
+				chain: Coin{Name: "Custom Coin"},
+			},
+			want:    "",
+			wantErr: true,
+		},
+		{
+			name: "Test Tron TRC10",
+			args: args{
+				addr:  "10token",
+				chain: Tron(),
+			},
+			want:    "https://tronscan.io/#/token/10token",
+			wantErr: false,
+		},
+		{
+			name: "Test Tron TRC20",
+			args: args{
+				addr:  "token",
+				chain: Tron(),
+			},
+			want:    "https://tronscan.io/#/token20/token",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetCoinExploreURL(tt.args.chain, tt.args.addr)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetCoinForId() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetCoinForId() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
