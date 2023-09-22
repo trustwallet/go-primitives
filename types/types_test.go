3e6d76730808d9b3efa8186d0d368ab8c739e964
daniel <142878302+tw-daniel@users.noreply.github.com>
2023-09-22 17:57:57 +0900

    Add more test cases

--- types/types_test.go
+++ types/types_test.go
@@ -10,13 +10,34 @@ import (
 
 func TestHex_UnmarshalAndMarshalJSON(t *testing.T) {
 	tests := []struct {
-		name   string
-		input  []byte
-		result string
+		name    string
+		input   []byte
+		result  string
+		isError bool
 	}{
 		{
-			input:  []byte(`{"value":"0x850a9af493d065b6c"}`),
-			result: "153386322112866048876",
+			name:    "value greater than 2^64 -1",
+			input:   []byte(`{"value":"0x850a9af493d065b6c"}`),
+			result:  "153386322112866048876",
+			isError: false,
+		},
+		{
+			name:    "value less than 2^64 -1",
+			input:   []byte(`{"value":"0x746a528800"}`),
+			result:  "500000000000",
+			isError: false,
+		},
+		{
+			name:    "error case: not hex (string)",
+			input:   []byte(`{"value":"error_value"}`),
+			result:  "",
+			isError: true,
+		},
+		{
+			name:    "error case: not hex (octal)",
+			input:   []byte(`{"value":"20502515364447501455554"}`),
+			result:  "",
+			isError: true,
 		},
 	}
 
@@ -29,6 +50,9 @@ func TestHex_UnmarshalAndMarshalJSON(t *testing.T) {
 			var v req
 
 			err := json.Unmarshal(tc.input, &v)
+			if tc.isError {
+				return
+			}
 			assert.NoError(t, err)
 
 			output := (*big.Int)(v.Value)
