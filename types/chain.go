3a6249d416a7a9d6b2ca7072d3f881897dd8ea67
Denis <136321897+defisaur@users.noreply.github.com>
2023-12-22 13:14:13 +0400

    opbnb

--- types/chain.go
+++ types/chain.go
@@ -151,7 +151,7 @@ func GetChainFromAssetType(assetType string) (coin.Coin, error) {
 		return coin.Cryptoorg(), nil
 	case COSMOS:
 		return coin.Cosmos(), nil
-	case OBNB:
+	case OPBNB:
 		return coin.Opbnb(), nil
 	case LINEA:
 		return coin.Linea(), nil
